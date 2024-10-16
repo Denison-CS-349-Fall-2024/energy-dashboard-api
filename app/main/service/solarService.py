8a66782a3ef21b385f6aae22d2cd68a19dc9c890
ducviettiendoan <ducviettiendoan@gmail.com>
2024-10-16 00:08:13 -0400

    migrate Flask to FastAPI for concurrency I/O based priority

--- app/main/service/solarService.py
+++ app/main/service/solarService.py
@@ -1,41 +1,44 @@
 import requests, os
 from flask import Response
 from dotenv import dotenv_values
+from ..model.ReponseModel import ReponseModel
 
 class SolarService:
     __config = dotenv_values(os.getcwd()+"/.env")
     def __init__(self):
         self.domain = SolarService.__config["SOLAR_ARRAY_URL"]
         
-    def get_site_by_key(self,api_key)->Response:
+    def get_site_by_key(self,api_key)->ReponseModel:
         params = {"api_key": api_key}
         try:
+            print("service layer make request")
             res = requests.get(self.domain+"sites/list", params=params)
             if res.status_code==200:
-                return Response(res,200,content_type="application/json")
-            return Response("Unauthorized",401)
+                return ReponseModel(message=res.json(),status=200)
+            return ReponseModel(message="Unauthorized",status=401)
         except:
-            return Response(str(requests.exceptions.HTTPError),500)
+            return ReponseModel(message=str(requests.exceptions.HTTPError),status=500)
     
-    def get_site_power(self,api_key,startTime,endTime,site_id)->Response:
+    def get_site_power(self,api_key,startTime,endTime,site_id)->ReponseModel:
         params = {"api_key": api_key, "startTime": startTime, "endTime": endTime}
+        print("service",params)
         try:
             res = requests.get(self.domain+f"site/{site_id}/power",params=params)
             if res.status_code==200:
-                return Response(res,200,content_type="application/json")
-            return Response("Unauthorized",401)
+                return ReponseModel(message=res.json(),status=200)
+            return ReponseModel(message="Unauthorized",status=401)
         except:
-            return Response(str(requests.exceptions.HTTPError),500)
+            return ReponseModel(str(requests.exceptions.HTTPError),status=500)
     
-    def get_site_energy(self,api_key,startDate,endDate,timeUnit,site_id)->Response:
+    def get_site_energy(self,api_key,startDate,endDate,timeUnit,site_id)->ReponseModel:
         params = {"api_key": api_key, "startDate": startDate, "endDate": endDate, "timeUnit": timeUnit}
         print(params)
         try:
             print(self.domain+f"site/{site_id}/energy")
             res = requests.get(self.domain+f"site/{site_id}/energy",params=params)
             if res.status_code==200:
-                return Response(res,200,content_type="application/json")
-            return Response("Unauthorized",401)
+                return ReponseModel(message=res.json(),status=200)
+            return ReponseModel(message="Unauthorized",status=401)
         except:
-            return Response(str(requests.exceptions.HTTPError),500)
+            return ReponseModel(message=str(requests.exceptions.HTTPError),status=500)
         
