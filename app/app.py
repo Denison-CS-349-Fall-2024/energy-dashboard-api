9a968fd9b25f2b2e374e8efa50b4d88e92329b6b
ducviettiendoan <ducviettiendoan@gmail.com>
2024-10-21 23:43:07 -0400

    check dependency name and test APIs successfully

--- app/app.py
+++ app/app.py
@@ -1,9 +1,9 @@
 from fastapi import FastAPI
-from app.main.controller.energyController import solarController
+from main.controller.energyController import energyController
 from main.controller.fireStoreController import firebaseController
 
 # create and configure the app
 app = FastAPI()
 # hook app with controllers
-app.include_router(solarController)
+app.include_router(energyController)
 app.include_router(firebaseController)
