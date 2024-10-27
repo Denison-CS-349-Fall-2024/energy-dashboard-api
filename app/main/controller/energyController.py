7fd96ab768f91da1741c66af6c865e39d035b955
ducviettiendoan <ducviettiendoan@gmail.com>
2024-10-27 17:17:11 -0400

    fix get_quick_insight to user_input cookie in request param

--- app/main/controller/energyController.py
+++ app/main/controller/energyController.py
@@ -81,23 +81,20 @@ class EnergyController():
             return ReponseModel(message=str(requests.exceptions.HTTPError),status=500)
 
     @energyController.get('/site/{id}/quick_insights/')
-    def get_quick_insights(id: int, quickInsightsType: str) -> ReponseModel:
+    def get_quick_insights(id: int, quickInsightsType: str, session_cookie:str) -> ReponseModel:
         try:
             site_api_key = EnergyController.__firebaseClient.getFireStoreDB(
                 EnergyController.__config["FIRESTORE_COLLECTION"],
                 EnergyController.__config["FIRESTORE_SOLAR_DOCUMENT"]
             )["solarEdgeKey"]
 
-            # call function the get cookie automatically instead of hardcode
-            cookie_string = "JSESSIONID=5B4D0396A34CCDCFA497170B072B1D2C; _gid=GA1.2.614391232.1729565242; cebs=1; org.springframework.web.servlet.i18n.CookieLocaleResolver.LOCALE=en; _ce.clock_data=11%2C140.141.4.65%2C1%2C09993ab868f470cf24e26fa4f9439d9e%2CChrome%2CUS; _gat_EPA=1; cebsp_=13; _ce.s=v~47a73f1dab4c828e0dd85e60bb5608c1834fbe87~lcw~1729718368932~vir~new~lva~1729565247386~vpv~0~v11.fhb~1729565248010~v11.lhb~1729718353983~v11.cs~170423~v11.s~74046390-9184-11ef-a886-ab8aa5eafe49~v11.sla~1729718369973~v11.send~1729718368541~lcw~1729718369973; _ga_2SEC4V3SK9=GS1.1.1729716557.6.1.1729718370.0.0.0; _ga_S0KJTVVLQ6=GS1.1.1729716557.6.1.1729718370.0.0.0; _ga_CSLL4ZEK4L=GS1.1.1729716557.6.1.1729718371.0.0.0; _ga=GA1.1.2096496035.1729565241"
-            
             # Retrieve insights based on the quickInsightsType
             if quickInsightsType == "solar":
                 res = EnergyController.__solarService.get_solar_insights(site_api_key, id)
             elif quickInsightsType == "electric":
-                res = EnergyController.__portfolioService.get_electric_insights(id, cookie_string)
+                res = EnergyController.__portfolioService.get_electric_insights(id, session_cookie)
             elif quickInsightsType == "natural_gas":
-                res = EnergyController.__portfolioService.get_gas_insights(id, cookie_string)
+                res = EnergyController.__portfolioService.get_gas_insights(id, session_cookie)
             else:
                 return ReponseModel(message="Invalid insights type", status=400)
             
