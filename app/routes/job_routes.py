25edec8384379e8a39a76ecfe001c16c4ad82ed4
Andrew Pham <locpp2001@gmail.com>
2024-12-08 15:06:33 -0500

    add schedulers

--- app/routes/job_routes.py
+++ app/routes/job_routes.py
@@ -1,3 +1,4 @@
+import aiohttp
 from fastapi import APIRouter, BackgroundTasks
 
 from app.jobs.scheduled_jobs import (
@@ -9,22 +10,30 @@ from app.jobs.scheduled_jobs import (
 router = APIRouter()
 
 
+@router.post("/trigger_hourly_job")
+async def trigger_hourly_job(background_tasks: BackgroundTasks):
+    background_tasks.add_task(refresh_selenium_session_cookie)
+    return {"message": "refresh_selenium_session_cookie sent in the background"}
+
+
 @router.post("/trigger_2am_job")
-def trigger_2am_job(background_tasks: BackgroundTasks):
+async def trigger_2am_job(background_tasks: BackgroundTasks):
     background_tasks.add_task(merge_sites)
-    print("2AM JOB ALERT: merge_sites sent in the background")
     return {"message": "merge_sites sent in the background"}
 
 
 @router.post("/trigger_3am_job")
-def trigger_3am_job(background_tasks: BackgroundTasks):
+async def trigger_3am_job(background_tasks: BackgroundTasks):
     background_tasks.add_task(update_site_images)
-    print("3AM JOB ALERT: update_site_images sent in the background")
     return {"message": "update_site_images sent in the background"}
 
 
-@router.post("/trigger_hourly_job")
-def trigger_hourly_job(background_tasks: BackgroundTasks):
-    background_tasks.add_task(refresh_selenium_session_cookie)
-    print("HOURLY JOB ALERT: refresh_selenium_session_cookie sent in the background")
-    return {"message": "refresh_selenium_session_cookie sent in the background"}
+async def trigger_fake_traffic():
+    print("Start faking traffic every 14 minutes to prevent Render from sleeping...")
+    url = "https://energy-dashboard-api-o69l.onrender.com/dummy_endpoint"
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            await response.json()
+
+    print("Finished faking traffic")
